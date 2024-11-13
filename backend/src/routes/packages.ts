686500e729a0b43212768c0486398aa8ae14c989
bitNathan <53419123+bitNathan@users.noreply.github.com>
2024-11-13 12:15:10 -0500

    added pagination

--- backend/src/routes/packages.ts
+++ backend/src/routes/packages.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-
-
-import { S3Client, ListObjectsV2Command } from '@aws-sdk/client-s3';
+import { S3Client, ListObjectsV2Command, ListObjectsV2Output } from '@aws-sdk/client-s3';
 
 
 const router = express.Router();
@@ -19,44 +17,72 @@ const s3Client = new S3Client({
 router.post('/packages', async (req, res) => {
   try {
     const {
-      offset = 1, // Provide this for pagination. If not provided, returns the first page of results
       Version,
       Name
     } = req.body;
+    const offset = req.query.offset || 1;
 
-    // TODO - Implement pagination via offset
-    // TODO version filtering
-    // TODO name filtering
-
-    // setup s3 bucket params
-    const params = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME as string
-    };
-
-    // get the list of objects in the bucket
-    const data = await s3Client.send(new ListObjectsV2Command(params));
-    
-    const packages = data.Contents;
-    
-    
-    // Check if there are any packages
-    if (!packages) {
-      res.status(404).json({message: 'No packages found'});
+    // Validate the offset
+    const offsetInt = parseInt(offset as string, 10);
+    if (isNaN(offsetInt)) {
+      res.status(400).json({ message: 'Invalid offset value' });
       return;
     }
+
+    // TODO version filtering
+    // TODO name filtering
     
+    // looping through pages
+    let page_number = 1;
+    const pageSize = 10;
+    let NextContinuationToken = null;
+    do {
+      let params = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME as string,
+        MaxKeys: pageSize,
+        ContinuationToken: NextContinuationToken ? NextContinuationToken : undefined,
+      };
 
-    // Build the response object
-    const response = {
-      packages: packages.map((p: any) => {
-        return {
-          Name: p.Key,
-          Version: p.Size,
-        };
-      }, []),
-    };
+      // get the list of objects in the bucket
+      const data: ListObjectsV2Output = await s3Client.send(new ListObjectsV2Command(params));
+      const packages = data.Contents;
+      // console.log(packages);  
+      
+      // Check if there are any packages
+      if (!packages) {
+        res.status(404).json({message: 'No packages found'});
+        return;
+      }
 
-    res.status(200).json(response);
+      // check if there are too many packages
+      if (packages.length > 1000) {
+        res.status(413).json({message: 'Too many packages found, use offset to paginate or filter'});
+        return;
+      }
+    
+      // Build the response object
+      const response = {
+        packages: packages.map((p: any) => {
+          return {
+            Name: p.Key,
+            Version: p.Size,
+          };
+        }, []),
+      };
+
+      if (page_number == offsetInt) {
+        res.status(200).json(response);
+        // console.log('response:', response);
+        return;
+      }
+      
+      // assign next token for paging
+      NextContinuationToken = data.NextContinuationToken;
+      page_number++;
+    
+    // should return before it gets here, but just in case
+    } while (page_number <= offsetInt);
+  
   } catch (error) {
     console.error('Error getting packages:', error);
     res.status(400).json({ 
